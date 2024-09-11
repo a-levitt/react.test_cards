f01e746a6cc498ae845d076f1e08bb41cc4695ba
Artem Trifonov <artemis.levitt@gmail.com>
2024-09-11 19:43:07 +0200

    Cards test

---
+++ src/Card/Card.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Card = (props) => {
+    return (
+      <div className="card">
+        <div className="top">
+          <h2 className="name">{props.name}</h2>
+          <img src={props.src} alt={props.alt} className="circle-img"/>
+        </div>
+        <div className="bottom">
+          <p className="info">{props.phone}</p>
+          <p className="info">{props.email}</p>
+        </div>
+      </div>
+    )
+}
+
+export default Card;
+
---
+++ src/index.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Card from "./Card/Card.jsx"
+
+ReactDOM.render(
+  <div>
+    <h1 className="heading">My Contacts</h1>
+    <Card 
+      name="Beyonce"
+      src="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
+      alt="avatar_beyonce"
+      phone="+123 456 789"
+      email="b@beyonce.com"
+    />
+    <Card 
+      name="Jack Bauer"
+      src="https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg"
+      alt="avatar_bauer"
+      phone="+987 654 321"
+      email="jack@nowhere.com"
+    />
+    <Card 
+      name="Chuck Norris"
+      src="https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png"
+      alt="avatar_norris"
+      phone="+918 372 574"
+      email="gmail@chucknorris.com"
+    />
+  </div>,
+  document.getElementById("root")
+);
+
+// If you're running this locally in VS Code use the commands:
+// npm install
+// to install the node modules and
+// npm run dev
+// to launch your react project in your browser
